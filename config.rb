ff839c03adf31ff6817c41c06a98b2ffca8593e2
moyashiki <moyashiki@gmail.com>
2016-01-02 23:16:46 +0900

    Chinachu側DBのファイルパスを移動したファイルパスに修正

--- config.rb
+++ config.rb
@@ -1,4 +1,5 @@
-USER = "henry"
+USER = "moyashiki"
 TEMPLATE = "$Title$/[$StTime$][$ChName$] $Title$ \#$Count$ $SubTitle$"
+RECORDED_DB = "/home/chinachu/Chinachu/data/recorded.json"
 CHANNEL = JSON.load(File.read(File.dirname(File.expand_path(__FILE__)) + "/channel.json"))
 REPLACE = JSON.load(File.read(File.dirname(File.expand_path(__FILE__)) + "/replace.json"))
